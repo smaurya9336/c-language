2963063827ebdf4d9bc3bc171aa6b1ea812e2b9e
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2021-08-28 17:02:50 +0530

    full pyramid

---
+++ pattern10.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main(){
+    int i,j,space,rows;
+    printf("Enter number of rows:--");
+    scanf("%d",&rows);
+    for(i=1;i<=rows-1;i++)
+    {
+        for(space=1;space<=(rows-i);space++)
+        {
+            printf(" ");
+        }
+        
+for ( j= 1; j<=i; j++)
+{
+    printf("%d ",i);
+}
+printf("\n");
+
+    }
+     for(i=rows;i>=1;i--)
+    {
+        for(space=1;space<=(rows-i);space++)
+        {
+            printf(" ");
+        }
+        
+for ( j= 1; j<=i; j++)
+{
+    printf("%d ",i);
+}
+printf("\n");
+
+    }
+
+}

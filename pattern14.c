9d78a74148f742af5f84d9b3a415ecb89ab42c3e
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2021-08-28 17:05:12 +0530

    Add files via upload

---
+++ pattern14.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+    int i,j,rows,sp;
+    printf("Enter number of rows:--");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+ for(sp=1;sp<=rows-i;sp++)
+{
+    printf(" ");
+}
+for(j=1;j<=2*i-1;j++){
+    printf("%d",i);
+}
+printf("\n");
+    }
+     for(i=rows-1;i>=1;i--)
+    {
+ for(sp=1;sp<=rows-i;sp++)
+{
+    printf(" ");
+}
+for(j=1;j<=2*i-1;j++){
+    printf("%d",i);
+}
+printf("\n");
+    }
+}

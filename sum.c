a97eb3a86a5b83cb3656a4b6967f38116391bb33
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2022-02-26 22:48:42 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a,b,c;
+    printf("enter two number:--");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("\n sum of two number is:%d",c);
+}
